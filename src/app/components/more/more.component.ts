9e8601cce69079f8ee045da06e639c0e61f1534c
6ustav9 <gustavoguedes95.gp15@gmail.com>
2023-03-08 19:56:09 -0300

    Ajuste sessao MORE

--- src/app/components/more/more.component.ts
+++ src/app/components/more/more.component.ts
@@ -15,19 +15,19 @@ export class MoreComponent {
             githubProject: "https://github.com/6ust/lyrics-redirect",
             description: "Angular JS com CSS3 - Disponibilizado por Netlify"
         },
-        {
-            link: "https://lyricsredirect.herokuapp.com",
-            name: "Lyrics Redirect Version 2",
-            githubUser: "6ust",
-            githubProject: "https://github.com/6ust/heroku-lyrics-angular",
-            description: "Angular 11 com CSS3 - Disponibilizado por Heroku"
-        },
+        // {
+        //     link: "https://lyricsredirect.herokuapp.com",
+        //     name: "Lyrics Redirect Version 2",
+        //     githubUser: "6ust",
+        //     githubProject: "https://github.com/6ust/heroku-lyrics-angular",
+        //     description: "Angular 11 com CSS3 - Disponibilizado por Heroku"    // Atualizar para a Vercel
+        // },
         {
             link: "https://lyrics-redirect.vercel.app",
-            name: "Lyrics Redirect Version 3",
+            name: "Lyrics Redirect Version 2",
             githubUser: "6ustav9",
             githubProject: "https://github.com/6ustav9/lyrics-redirect",
-            description: "Angular 14 com SASS, consumindo Json Server - Disponibilizado por Vercel e Heroku"
+            description: "Angular 14 com SASS, consumindo Json Server - Disponibilizado por Vercel"
         }
 
         
